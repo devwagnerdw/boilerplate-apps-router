a762dc118d87c792f999fd278d2c837c20eebc3a
Vagne alves <vagnerdw1@gmail.com>
2023-10-02 21:37:09 -0300

    Update Main component and add images

--- src/components/main/styles.ts
+++ src/components/main/styles.ts
@@ -1,8 +1,6 @@
 'use client'
-
-import { styled } from 'styled-components'
-
-export const Whapper = styled.main`
+import styled from 'styled-components'
+export const Wrapper = styled.main`
   background-color: #06092b;
   color: #fff;
   width: 100%;
@@ -13,4 +11,23 @@ export const Whapper = styled.main`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+`
+
+export const Logo = styled.img`
+  width: 25rem;
+  margin-bottom: 2rem;
+`
+
+export const Title = styled.h1`
+  font-size: 2.5rem;
+`
+
+export const Description = styled.h2`
+  font-size: 2rem;
+  font-weight: 400;
+`
+
+export const Illustration = styled.img`
+  margin-top: 3rem;
+  width: min(30rem, 100%);
 `
